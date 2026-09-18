9ede1eefcf102dbd3a1c431d22cd97779c5a6e2f
NandishAchhinagoudar <nandishrachhinagoudar@gmail.com>
2026-09-18 11:48:42 +0530

    Add basic arithmetic operations in prog4.py

---
+++ prog4.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter the first number:"))
+num2 = int(input(enter the second number"))
+addition = num1 + num2
+substraction = num1 - num2
+print("addition: {num1} + {num2} = {addition}")
+print("substraction: {num1} - {num2} = {subtraction}")
